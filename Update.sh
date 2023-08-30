430790c8faa57468e76316073fccc02a1e7bacb3
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-08-30 15:03:55 -0400

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,19 +1,20 @@
 Nalani Daniels
 August 30, 2023
 
-# Purpose:
+Purpose:
 Create a script called buildscript1.sh that updates the server, creates a file that indicates how many packages can be upgraded, and saves that output to a file titled 'update$date.txt'
 
-# Script:
+Script:
+#Indicate this file is a bash script
 #!/bin/bash
 
 #Updates the server's repository, then uses the grep command to find where the output specifies how many packages can be upgraded. The cut command simply carves out the relevant information, and then the output is redirected to a file with the date.
 sudo apt update | grep "upgraded" | cut -f 1 -d "." > update$(date +"%m.%d.%Y").txt
 
-# Changing Permissions
+Changing Permissions
 chmod 700 buildscript1.sh
 
-# Setting Up Cronjob
+Setting Up Cronjob
 #Open the cronjob file for my user ubuntu
 sudo crontab -e
 
