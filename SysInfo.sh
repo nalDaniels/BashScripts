6739cca157d3da294af0d249f5cf7a904f9dc0af
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-09-11 11:44:26 -0400

    Update SysInfo.sh

--- SysInfo.sh
+++ SysInfo.sh
@@ -2,8 +2,9 @@
 
 #Create a menu to display system information 
 echo "System Information Menu. Choose 1-13"
-select option in "Currently Logged User" "Your Shell Directory" "Home Directory" "OS Name & Version" "Current Working Directory" "Number of Users Logged In" "Hard Disk Information" "CPU Information" "Memory Information" "File System Information" "Current Running Process"
+select option in "Currently Logged User" "Your Shell Directory" "Home Directory" "OS Name & Version" "Current Working Directory" "Number of Users Logged In" "Hard Disk Information" "CPU Information" "Memory Information" "File System Information" "Current Running Process" "Exit"
 do
+  case 
   
 
 
