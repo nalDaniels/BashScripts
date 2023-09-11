b65138f204632042f48b82923f7619d7174492bb
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-09-11 11:45:58 -0400

    Update SysInfo.sh

--- SysInfo.sh
+++ SysInfo.sh
@@ -2,9 +2,11 @@
 
 #Create a menu to display system information 
 echo "System Information Menu. Choose 1-13"
-select option in "Currently Logged User" "Your Shell Directory" "Home Directory" "OS Name & Version" "Current Working Directory" "Number of Users Logged In" "Hard Disk Information" "CPU Information" "Memory Information" "File System Information" "Current Running Process" "Exit"
+select option in "Current Logged User" "Your Shell Directory" "Home Directory" "OS Name & Version" "Current Working Directory" "Number of Users Logged In" "Hard Disk Information" "CPU Information" "Memory Information" "File System Information" "Current Running Process" "Exit"
 do
-  case 
+  #Create a case statement that executes commands based on system information menu selection
+  case $option in
+    "Current Logged User" )
   
 
 
