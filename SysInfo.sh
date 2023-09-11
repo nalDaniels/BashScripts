15588bb388f5827712a9ead26033207f43179437
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-09-11 11:42:29 -0400

    Update and rename SysInfo to SysInfo.sh

---
+++ SysInfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Create a menu to display system information 
+echo "System Information Menu. Choose 1-13"
+select option in "Currently Logged User" "Your Shell Directory" "Home Directory" "OS Name & Version" "Current Working Directory" "Number of Users Logged In" "Hard Disk Information" "CPU Information" "Memory Information" "File System Information" "Current Running Process"
+do
+  
+
+
